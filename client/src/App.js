57896499b3e2bc42efc19f572bba2ea15f5afe3c
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-07-09 12:22:30 -0500

    fish and vamp thoughts rendering

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
