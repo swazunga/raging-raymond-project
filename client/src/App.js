05dc82272435d475a913c90b476a198968f98cb6
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-07-09 12:21:44 -0500

    fish and vamp thoughts rendering at app

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+
+
+import './App.css';
+import FishTopic from './Pages/FishTopic';
+import VampTopic from './Pages/VampTopic';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <FishTopic />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/VampTopic.js
@@ -0,0 +1,26 @@
+import { useQuery } from '@apollo/client';
+import { QUERY_VAMPTOPICS } from '../utils/queries';
+import VampTopicList from '../components/VampTopicList';
+
+const VampTopic = () => {
+    //use useQuery hook to make query request
+    const { loading, data } = useQuery(QUERY_VAMPTOPICS);
+    const vampTopics = data?.vampTopics || [];
+    console.log(vampTopics)
+
+    return (
+        <main>
+            <div className='flex-row justify-space-between'>
+                <div className='col-12 mb-3'>
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <VampTopicList vampTopics={vampTopics} title="Some Vamp Chats..." />
+                    )}
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default VampTopic;
