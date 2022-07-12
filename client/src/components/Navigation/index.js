68e7fe5e00b6d7acf4644e6006d32c92fdeff946
Michael Kendrick <michael@locumsmart.net>
2022-07-11 21:15:01 -0500

    nav bar and registration page started

---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <div>
+      <ul className="nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#About"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#FishTopic"
+            onClick={() => handlePageChange("FishTopic")}
+            className={
+              currentPage === "FishTopic" ? "nav-link active" : "nav-link"
+            }
+          >
+            Fish Topics
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#VampTopic"
+            onClick={() => handlePageChange("VampTopic")}
+            className={
+              currentPage === "VampTopic" ? "nav-link active" : "nav-link"
+            }
+          >
+            Vamp Topics
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Registration"
+            onClick={() => handlePageChange("Registration")}
+            className={
+              currentPage === "Registration" ? "nav-link active" : "nav-link"
+            }
+          >
+            Register for Fishing Tournament
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Donations"
+            onClick={() => handlePageChange("Dontations")}
+            className={
+              currentPage === "Donations" ? "nav-link active" : "nav-link"
+            }
+          >
+            Donate!
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ client/src/components/Registration/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
+function Registration() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    participants: "",
+  });
+  const { name, email, participants } = formState;
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <section>
+      <h1 data-testid="h1tag">Fluke Tournament Registration</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="participants">Participants:</label>
+          <textarea
+            name="participants"
+            rows="1"
+            defaultValue=""
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Registration;
