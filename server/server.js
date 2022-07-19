96ce69546c761d9b231295ca6be2f276ead16a4d
Michael Kendrick <michael@locumsmart.net>
2022-07-19 14:16:30 -0500

    changing if statement location

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const path = require("path");
 
-const { authMiddleware } = require('./utils/auth')
+const { authMiddleware } = require("./utils/auth");
 
 //import Apollo server
 
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 //import typeDefs and Resolvers
 
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
 //create a new Apollo server and pass in schema data
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
@@ -30,28 +30,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //create new instance of Apollo Server with GQL schema
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
+  await server.start();
 
-    //integrate Apollo server with Express app and middleware
-    server.applyMiddleware({ app });
+  //integrate Apollo server with Express app and middleware
+  server.applyMiddleware({ app });
 
+  //Serve up staic assets
 
-    //Serve up staic assets
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
-    }
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port${PORT}!`);
     });
-
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port${PORT}!`);
-        })
-    })
+  });
 };
 
 //call async function to start server
