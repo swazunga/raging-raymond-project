8e247dcbed79f7b55335bff4fc33b42cd7c3b0e0
Michael Kendrick <michael@locumsmart.net>
2022-07-19 17:18:50 -0500

    adding build

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   }
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
   db.once("open", () => {
     app.listen(PORT, () => {
