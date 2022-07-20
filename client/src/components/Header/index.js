7197ca62669ad519da67185563b0361e301988b1
Matthew Wiser <matt.wiser@icloud.com>
2022-07-20 00:59:27 -0500

    continued refinement of navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Header = () => {
@@ -12,30 +13,34 @@ const Header = () => {
         <header className="navbar bg-light">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">
-                    <img src="/images/fish.png" className="d-inline-block align-text-top nav-icon"/>
+                    <img src="/images/fish.png" className="d-inline-block nav-icon"/>
                     <span className="nav-name">Raging Raymond</span>
                 </NavLink>
                 <nav className="nav">
                     {Auth.loggedIn() ? (
                         <>
-                            <NavLink to={`/profile`} className="nav-link">My Profile</NavLink>
-                            <NavLink to={`/`} onClick={logout} className="nav-link">Log Out</NavLink>
-                            {/* <a href="/" onClick={logout}>
-                                Logout
-                            </a> */}
+                            <NavLink to={`/profile`} className="nav-link">My Profile<span></span></NavLink>
                         </>
                     ) : (
                         <>
-                            <NavLink to='/login' className="nav-link">Login</NavLink>
-                            <NavLink to='/signup' className="nav-link">Signup</NavLink>
                         </>
                     )}
 
-                    <NavLink to='/gallery' className="nav-link">Gallery</NavLink>
-                    <NavLink to='/donate' className="nav-link">Donate</NavLink>
-                    <NavLink to='/fishTopics' className="nav-link">Fish Tales</NavLink>
-                    <NavLink to='/vampTopics' className="nav-link">Vamp Chat</NavLink>
-                    <NavLink to='/registration' className="nav-link">Registration</NavLink>
+                    <NavLink to='/gallery' className="nav-link">Gallery<span></span></NavLink>
+                    <NavLink to='/donate' className="nav-link">Donate<span></span></NavLink>
+                    <NavLink to='/fishTopics' className="nav-link">Fish Tales<span></span></NavLink>
+                    <NavLink to='/vampTopics' className="nav-link">Vamp Chat<span></span></NavLink>
+                    <NavLink to='/registration' className="nav-link">Tournament Registration<span></span></NavLink>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to={``} onClick={logout} className="nav-link">Log Out<span></span></Link>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink to='/login' className="nav-link">Login<span></span></NavLink>
+                            <NavLink to='/signup' className="nav-link">Signup<span></span></NavLink>
+                        </>
+                    )}
                 </nav>
             </div>
         </header>
