c01444333d5eaf9073dafd15b5a38a8a867334d8
Matthew Wiser <matt.wiser@icloud.com>
2022-07-19 23:06:45 -0500

    refactored navbar

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 const About = () => {
   return (
-      <div className="about-container">
+      <>
       <Hero/>
       <div className="container">
         <div className="row justify-content-evenly">
@@ -42,7 +42,7 @@ const About = () => {
           </div>
         </div>
       </div>
-    </div>
+      </>
   );
 };
 
